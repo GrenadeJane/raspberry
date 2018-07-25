e15166ad6465f4afc476a432b56f092b45a1fcf9
Léa <heilea@hotmail.fr>
2018-07-25 15:49:02 +0200

    create server and running it

---
+++ app.js
@@ -0,0 +1,35 @@
+'use strict'
+const Groceries = require('./routes/mongoose.js');
+var logger = require('morgan');
+var bodyParser = require('body-parser');
+var app = express();
+var http = require('http');
+
+//dom(app); 
+// :: Dependencies in the personal code
+
+app.use(bodyParser.json({ limit: '50mb' }));
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+
+
+app.get('/test', (req, res) => {
+	res.send( 200, {message : "hello world "});
+});
+
+
+ http.createServer(function (req, res) {
+	  res.writeHead(200, {'Content-Type': 'text/plain'}); 
+	  res.end('Hello World\n'); 
+	}).listen(80);
+console.log('Server running');
---
+++ routes/mongoose.js
@@ -0,0 +1,24 @@
+// mdp : raspberry6 usr: root
+var mongoose = require('mongoose');
+
+let uri;
+uri = 'mongodb://root:raspberry6@ds247191.mlab.com:47191/raspberry-grenade'; 
+
+mongoose.connect(uri,  { useNewUrlParser: true } )
+	.then(() => console.log('Connected to MongoDB ... '))
+	.catch(err => console.log('Could not connect to MongoDB ...', err));
+
+	const Groceries = mongoose.model('Groceries', new mongoose.Schema({
+		sender : {
+			type : String, 
+			min : 3, 
+			max :25 
+		},
+		list : {
+			type : Array(String),
+			
+		},
+		price : Number
+	}));
+
+module.exports = Groceries; 
