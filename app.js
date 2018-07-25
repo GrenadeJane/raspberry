c6392eddb2822880a49042043e5a19f6cf1189aa
Léa <heilea@hotmail.fr>
2018-07-25 16:00:54 +0200

    remove all

--- app.js
+++ app.js
@@ -9,24 +9,24 @@ var http = require('http');
 //dom(app); 
 // :: Dependencies in the personal code
 
-app.use(bodyParser.json({ limit: '50mb' }));
-app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+// app.use(bodyParser.json({ limit: '50mb' }));
+// app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
+// app.use(express.static(path.join(__dirname, 'public')));
+// app.use(function (err, req, res, next) {
+//   // set locals, only providing error in development
+//   res.locals.message = err.message;
+//   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+//   // render the error page
+//   res.status(err.status || 500);
+//   res.render('error');
+// });
 
 
 
-app.get('/test', (req, res) => {
-	res.send( 200, {message : "hello world "});
-});
+// app.get('/test', (req, res) => {
+// 	res.send( 200, {message : "hello world "});
+// });
 
 
  http.createServer(function (req, res) {
