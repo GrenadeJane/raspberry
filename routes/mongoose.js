3e6d912cdc8288700788e4f66f36138b7f7e6dcd
Léa <heilea@hotmail.fr>
2018-07-25 16:03:32 +0200

    comment mognoose

--- routes/mongoose.js
+++ routes/mongoose.js
@@ -1,26 +1,26 @@
 // mdp : raspberry6 usr: root
 
-'use strict'
-var mongoose = require('mongoose');
+// 'use strict'
+// var mongoose = require('mongoose');
 
-let uri;
-uri = 'mongodb://root:raspberry6@ds247191.mlab.com:47191/raspberry-grenade'; 
+// let uri;
+// uri = 'mongodb://root:raspberry6@ds247191.mlab.com:47191/raspberry-grenade'; 
 
-mongoose.connect(uri,  { useNewUrlParser: true } )
-	.then(() => console.log('Connected to MongoDB ... '))
-	.catch(err => console.log('Could not connect to MongoDB ...', err));
+// mongoose.connect(uri,  { useNewUrlParser: true } )
+// 	.then(() => console.log('Connected to MongoDB ... '))
+// 	.catch(err => console.log('Could not connect to MongoDB ...', err));
 
-	const Groceries = mongoose.model('Groceries', new mongoose.Schema({
-		sender : {
-			type : String, 
-			min : 3, 
-			max :25 
-		},
-		list : {
-			type : Array(String),
+// 	const Groceries = mongoose.model('Groceries', new mongoose.Schema({
+// 		sender : {
+// 			type : String, 
+// 			min : 3, 
+// 			max :25 
+// 		},
+// 		list : {
+// 			type : Array(String),
 			
-		},
-		price : Number
-	}));
+// 		},
+// 		price : Number
+// 	}));
 
-module.exports = Groceries; 
+// module.exports = Groceries; 
